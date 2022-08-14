11db10765e5bc64d01b638ba579131377185f49d
SwizzyLM <102028750+SwizzyLM@users.noreply.github.com>
2022-08-15 00:30:57 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -41,10 +41,6 @@ guildsButton.onclick = function () {
 
 function removeCards() {
     setTimeout(function () {
-        container.style.opacity = '0'
-        btns.style.opacity = '0'
-        avatar.style.opacity = '0'
-        block.style.opacity = '0'
     }, 0)
 
     guilds.style.display = 'block'
